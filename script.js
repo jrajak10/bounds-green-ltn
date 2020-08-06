bda503df57b9e72909313b79010383026a0dd82c
Johann.Rajakarunanayake <jrajakarunanayake@gmail.com>
2020-08-06 14:10:57 +0100

    added traffic affected roads

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var API_KEY = 'Ae6wi8HZzLBBiZE3xLoRvqYhqoV83ije';
+let API_KEY = 'Ae6wi8HZzLBBiZE3xLoRvqYhqoV83ije';
 
-var wfsServiceUrl = 'https://api.os.uk/features/v1/wfs',
+let wfsServiceUrl = 'https://api.os.uk/features/v1/wfs',
     tileServiceUrl = 'https://api.os.uk/maps/raster/v1/zxy';
 
-    let vectorUrl = 'https://api.os.uk/maps/vector/v1/vts';
+let vectorUrl = 'https://api.os.uk/maps/vector/v1/vts';
 
 // Create a map style object using the OS Maps API ZXY service.
-var style = {
+let style = {
     'version': 8,
     'sources': {
         'raster-tiles': {
@@ -24,15 +24,15 @@ var style = {
 };
 
 // Initialize the map object.
-var map = new mapboxgl.Map({
+let map = new mapboxgl.Map({
     container: 'map',
     minZoom: 12,
     maxZoom: 20,
     style: vectorUrl + '/resources/styles?key=' + API_KEY,
-    center: [-0.1304205, 51.6086152],
+    center: [-0.1241096, 51.6098585],
     zoom: 15,
     transformRequest: url => {
-        if(! /[?&]key=/.test(url) ) url += '?key=' + API_KEY
+        if (! /[?&]key=/.test(url)) url += '?key=' + API_KEY
         return {
             url: url + '&srs=3857'
         }
@@ -55,7 +55,7 @@ function convertLineStringCoords(arr) {
     return arr;
 }
 
-function addStreetsLayer(features, id, color) {
+function addStreetsLayer(features, id, color, width) {
     map.addSource(id, {
         'type': 'geojson',
         'data': {
@@ -69,72 +69,88 @@ function addStreetsLayer(features, id, color) {
         'type': 'line',
         'source': id,
         'paint': {
-            'line-width': 10,
+            'line-width': width,
             'line-color': color
         }
     });
 }
 
+async function fetchRoads(roads){
+    let totalArray = [];
+    for (let i = 0; i < roads.length; i++) {
+        totalArray.push(await getFeatures('Highways_Roadlink', roads[i]))
+    }
+    return totalArray
+}
+
+function convertAndMerge(array){
+    let convertedArrayCoords = array.map(x => convertLineStringCoords(x));
+    let mergedArrayCoords = [].concat(...convertedArrayCoords);
+    return mergedArrayCoords;
+}
+
 // Add event which waits for the map to be loaded.
 map.on('load', async function () {
 
-    let westbury = await getFeatures('Highways_Roadlink', 'Westbury Road');
+    map.on('click', function (e) {
+
+
+        let bounds = map.getBounds();
+
+        let x = bounds.getSouthWest().lat + ',' + bounds.getSouthWest().lng,
+            y = bounds.getNorthEast().lat + ',' + bounds.getNorthEast().lng;
+
+        let z = x + ' ' + y;
+        console.log(JSON.stringify(e.lngLat.wrap()))
+    });
+
+    let roads = ['Westbury Road', 'Brownlow Road', 'Elvendon Road', "Goring Road", "Beech Road",
+        "Hardwicke Road", "Natal Road", "York Road", "Warwick Road", "Highworth Road",
+        "Stanley Road", "Ollerton Road", "Evesham Road", "Shrewsbury Road", "Maidstone Road",
+        "Teweskbury Terrace", "Russell Road", "Whittington Road", "Palmerston Road"]
+
+    let trafficRoadsArray = ['Bounds Green Road', 'Bowes Road', "Green Lanes", "High Road", "Telford Road", 
+                            "Durnsford Road", "Powys Lane", "Wilmer Way"]
+
+    
+    let trafficRoads = await fetchRoads(trafficRoadsArray)
+    let mergedTrafficRoads = convertAndMerge(trafficRoads)
+   
     let brownlow = await getFeatures('Highways_Roadlink', 'Brownlow Road');
-    let elvendon = await getFeatures('Highways_Roadlink', 'Elvendon Road');
-    let goring = await getFeatures('Highways_Roadlink', 'Goring Road');
-    console.log(goring)
 
     let bannedStreets = [[brownlow[1]], [brownlow[5]], [brownlow[7]], [brownlow[15]], [brownlow[16]]]
-    console.log(bannedStreets)
-    let convertedBannedCoords = bannedStreets.map(x => convertLineStringCoords(x));
-    let mergedBannedStreets = [].concat(...convertedBannedCoords)
-
-
-
-    let allowedBrownlow;
-    for(let i=0; i<brownlow.length; i++){
-        if(i===1 || i=== 5|| i=== 7 || i=== 15 || i===16){
-            delete brownlow[i]
-        }
-        allowedBrownlow = brownlow.filter(Boolean);
-    }
-
-    
-
-    let allowedStreets = [westbury, allowedBrownlow, elvendon, [goring[2]]]
+    let mergedBannedStreets = convertAndMerge(bannedStreets);
+ 
+    let allowedStreets = await fetchRoads(roads);
     console.log(allowedStreets)
+    let mergedAllowedStreets = convertAndMerge(allowedStreets);
 
 
-    // console.log(uniqueStreets)
-    let convertedAllowedStreetCoords = allowedStreets.map(x => convertLineStringCoords(x))
-    let mergedAllowedStreets = [].concat(...convertedAllowedStreetCoords)
-
-
+    addStreetsLayer(mergedAllowedStreets, 'allowed-streets', '#00ab66', 5)
+    addStreetsLayer(mergedBannedStreets, 'banned-streets', '#FFA500', 10)
+    addStreetsLayer(mergedTrafficRoads, 'traffic-streets', '#F00', 10)
     
-
-    addStreetsLayer(mergedAllowedStreets, 'allowed-streets', '#00ab66')
-    addStreetsLayer(mergedBannedStreets, 'banned-streets', '#FFA500')
 
 
 
     // Get the visible map bounds (BBOX).
-    var bounds = map.getBounds();
+    let bounds = map.getBounds();
 
     function click(id) {
         // When a click event occurs on a feature in the 'streets' layer, open a popup at
         // the location of the click, with description HTML from its properties.
         map.on('click', id, function (e) {
 
-            var bounds = map.getBounds();
+            let bounds = map.getBounds();
 
-            var x = bounds.getSouthWest().lat + ',' + bounds.getSouthWest().lng,
+            let x = bounds.getSouthWest().lat + ',' + bounds.getSouthWest().lng,
                 y = bounds.getNorthEast().lat + ',' + bounds.getNorthEast().lng;
 
-            var z = x + ' ' + y;
+            let z = x + ' ' + y;
 
             new mapboxgl.Popup()
                 .setLngLat(e.lngLat)
-                .setHTML(e.features[0].properties.RoadName1)
+                .setHTML(e.features[0].properties.RoadName1 + ' '+ e.features[0].properties.RoadClassification )
                 .addTo(map);
         });
     }
@@ -154,6 +170,7 @@ map.on('load', async function () {
     }
 
     click('allowed-streets')
+    click('traffic-streets')
     click('banned-streets')
     mouseEnter('allowed-streets')
     mouseEnter('banned-streets')
@@ -167,13 +184,13 @@ map.on('load', async function () {
  */
 async function getFeatures(typeName, literal) {
     // Convert the bounds to a formatted string.
-    let sw = [51.6050104126017, -0.14634209284295707],
-        ne = [51.615524130859825, -0.11544304499136615];
+    let sw = [51.59536880893367, -0.13772922026373635],
+        ne = [51.61765036734033, -0.08604524597762975];
 
     let coords = sw + ' ' + ne;
     // Create an OGC XML filter parameter value which will select the Airport
     // features (site function) intersecting the BBOX coordinates.
-    var xml = '<ogc:Filter>';
+    let xml = '<ogc:Filter>';
     xml += '<ogc:And>';
     xml += '<ogc:BBOX>';
     xml += '<ogc:PropertyName>SHAPE</ogc:PropertyName>';
@@ -225,7 +242,7 @@ async function getFeatures(typeName, literal) {
  * @param {object} params - The parameters object to be encoded.
  */
 function getUrl(params) {
-    var encodedParameters = Object.keys(params)
+    let encodedParameters = Object.keys(params)
         .map(paramName => paramName + '=' + encodeURI(params[paramName]))
         .join('&');
 
