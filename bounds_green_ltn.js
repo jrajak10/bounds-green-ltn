7f77c87762f101ae5b15bc5436f21c2e71225795
Johann.Rajakarunanayake <jrajakarunanayake@gmail.com>
2020-08-08 16:45:53 +0100

    added road issues

--- bounds_green_ltn.js
+++ bounds_green_ltn.js
@@ -30,8 +30,8 @@ let map = new mapboxgl.Map({
     minZoom: 12,
     maxZoom: 20,
     style: vectorUrl + '/resources/styles?key=' + API_KEY,
-    center: [-0.1241096, 51.6098585],
-    zoom: 15,
+    center: [-0.1188682761282962, 51.60766464643319],
+    zoom: 14,
     transformRequest: url => {
         if (! /[?&]key=/.test(url)) url += '?key=' + API_KEY
         return {
---
+++ road_issues.js
@@ -0,0 +1,121 @@
+
+
+export const TRAFFIC_ROAD_ISSUES = {
+    "Bounds Green Road": "Drivers unable to turn right into Brownlow Road would need to continue straight through " +
+        "Bounds Green Road. The increase in traffic will result in an increase in pollution, which" +
+        "will affect staff and students in the nearby schools - Bounds Green School and Trinity Primary Academy",
+    "Bowes Road": "Bowes Road will see an increase in congestion if the LTN goes ahead. In addition, the increase " +
+        "in vehicles driving through will result in an increase in air and noise pollution, which will affect the " +
+        "schools close by - Bowes School and Our Lady of Lourdes School. Furthermore, Ambulances travelling to and from " +
+        "North Middlesex Hospital use the A406 as their primary source of transportation. When there is an " +
+        "increase in traffic, there will be an increase in journey time. In some emergencies, this increase " +
+        "is a matter of life and death.",
+    "Green Lanes": "Traffic will increase as a result of people driving Nortbound towards Southgate/Palmers Green, and " +
+        "Southbound towards Bounds Green/Muswell Hill, but are unable to do so using Brownlow Road. According to a " +
+        "<a href = \"https://www.haringey.gov.uk/sites/haringeygovuk/files/green_lane_study_report_final.pdf\">study</a>, " +
+        "Nitrogen dioxide levels already exceeds the EU limit in this area.",
+    "High Road": "Traffic will increase as a result of people driving Nortbound towards Southgate/Palmers Green, and " +
+        "Southbound towards Bounds Green/Muswell Hill, but are unable to do so using Brownlow Road. According to a " +
+        "<a href = \"https://www.haringey.gov.uk/sites/haringeygovuk/files/green_lane_study_report_final.pdf\">study</a>, " +
+        "Nitrogen dioxide levels already exceeds the EU limit in this area.",
+    "Telford Road": "Telford Road (part of the A406) was already ranked as the 6th most congested road in the UK, with an estimated 43 hours lost per year. " +
+        "Closing off Brownlow Road for vehicles will only increase the congestion, and journey times for those driving " +
+        "through the A406, many of whom are transporting goods across the country",
+    "Durnsford Road": "Residents living in the majority of the affected roads would need to drive all the way round " +
+        "Bounds Green Road/A406 in order to get back to their homes, resulting in increased journey times " +
+        "and fuel spent. This road is already congested in the evening rush hour, and will only be " +
+        "more so. Furthermore, the increase in traffic will result in an increase in pollution, which will " +
+        "affect students and staff in the nearby schools - Alexandra Park School and Rhodes Avenue School.",
+    "Powys Lane": "People travelling through Powys Lane wishing to travel towards Bounds Green/Muswell Hill would have to " +
+        "drive through an already congested North Circular Road in order to get there, instead of going straight " +
+        "through Brownlow Road, resulting in an increase in journey time and fuel used.",
+    "Wilmer Way": "There will be an increase in traffic from people driving towards Bounds Green, Muswell Hill and Southgate, but unable to " +
+        "use Brownlow Road. This will also result in an increase in pollution, affecting students and staff in " +
+        "Broomfield School",
+    "Pinkham Way": "Pinkham Way (part of the A406) was already ranked as the 6th most congested road in the UK, with an estimated 43 hours lost per year. " +
+        "Closing off Brownlow Road for vehicles will only increase the congestion, and journey times for those driving " +
+        "through the A406, many of whom are transporting goods across the country",
+    "North Circular Road": "North Circular Road (A406) between East Finchley and Edmonton was already ranked as the 6th " +
+        "most congested road in the UK, with an estimated 43 hours lost per year. " +
+        "Closing off Brownlow Road for vehicles will only increase the congestion, and journey times for those driving " +
+        "through the A406, many of whom are transporting goods across the country. " +
+        "Furthermore, Ambulances travelling to and from " +
+        "North Middlesex Hospital use the A406 as their primary source of transportation. When there is an " +
+        "increase in traffic, there will be an increase in journey time. In some emergencies, this increase " +
+        "is a matter of life and death."
+};
+
+export const RESIDENTIAL_ROAD_ISSUES = {
+    "Westbury Road": "With the proposed banning of residential vehicles on Brownlow Road, combined with the closure of the " +
+        "bottom of Westbury Road, residents living here will be 'trapped', and unable to drive to and from " +
+        "their homes. There has been no clarity over how this issue is to be addressed. Many residents are elderly, " +
+        "and rely on cars for travelling. This proposal will also make roads less accessible for disabled people. " +
+        "In addition, there is a good chance house prices would drop as people are less likely to buy in areas " +
+        "if they are unable to drive to and from home.",
+    "Brownlow Road": "Test",
+    "Elvendon Road": "With the proposed banning of residential vehicles on Brownlow Road, combined with the closure of the " +
+        "bottom of Westbury Road, residents living here will be 'trapped', and unable to drive to and from " +
+        "their homes. There has been no clarity over how this issue is to be addressed. Many residents are elderly, " +
+        "and rely on cars for travelling. This proposal will also make roads less accessible for disabled people. " +
+        "In addition, there is a good chance house prices would drop as people are less likely to buy in areas " +
+        "if they are unable to drive to and from home.",
+    "Goring Road": "With the proposed banning of residential vehicles on Brownlow Road, residents living here will be 'trapped', and unable to drive to and from " +
+        "their homes. There has been no clarity over how this issue is to be addressed. Many residents are elderly, " +
+        "and rely on cars for travelling. This proposal will also make roads less accessible for disabled people. " +
+        "In addition, there is a good chance house prices would drop as people are less likely to buy in areas " +
+        "if they are unable to drive to and from home.",
+    "Beech Road": "With the proposed banning of residential vehicles on Brownlow Road, combined with the closure of the " +
+        "bottom of Westbury Road, residents living here will be 'trapped', and unable to drive to and from " +
+        "their homes. There has been no clarity over how this issue is to be addressed. Many residents are elderly, " +
+        "and rely on cars for travelling. This proposal will also make roads less accessible for disabled people. " +
+        "In addition, there is a good chance house prices would drop as people are less likely to buy in areas " +
+        "if they are unable to drive to and from home.",
+    "Hardwicke Road": "With the proposed banning of residential vehicles on Brownlow Road, combined with the closure of the " +
+        "bottom of Westbury Road, residents living here will be 'trapped', and unable to drive to and from " +
+        "their homes. There has been no clarity over how this issue is to be addressed. Many residents are elderly, " +
+        "and rely on cars for travelling. This proposal will also make roads less accessible for disabled people. " +
+        "In addition, there is a good chance house prices would drop as people are less likely to buy in areas " +
+        "if they are unable to drive to and from home.",
+    "Natal Road": "There will be more residents driving through here from the A406, as York and Brownlow Road have been " +
+        "blocked off. Furthermore, as Natal Road is far narrower than Brownlow Road, congestion with " +
+        "emergency vehicles could result in delays. Residents living closer to the A406 will be affected " +
+        "by increased pollution.",
+    "York Road": "The bottom of York Road is to be blocked off, meaning residents would have to travel through the A406 " +
+        "to get to and from home, which is an unnecessary higher risk.",
+    "Warwick Road": "<img src = \"warwick_road.png\"><br>" + "As the bottom of Warwick Road is blocked off, residents " +
+        "of nearby roads travelling from Bounds Green would need to travel through the A406, and take a high " +
+        "risk right turning (shown above) into Warwick Road, attempting to evade speeding vehicles. There will be " +
+        "more congestion from vehicles driven by residents from nearby roads. Narrow roads will also make increase " +
+        "journey times for emergency vehicles.",
+    "Highworth Road": "More residents from Shrewsbury Road will be driving through here, as York Road is blocked off. " +
+        "An increase in traffic from the A406 will affect students and staff at Bowes School.",
+    "Stanley Road": "Residents here would have no alternative but to travel via the A406 for all their journeys. " +
+        "What would previously have been a simple journey towards/through Bounds Green would now end up " +
+        "being like travelling through a maze as a result of road closures.",
+    "Ollerton Road": "Ollerton Road faces a similar issue to Stanley Road because there is residents cannot turn " +
+        "into the A406. For any Eastbound journeys, residents " +
+        "would need to take a high risk right turn through Warwick Road towards the A406 instead of " +
+        "travelling through Bounds Green.",
+    "Evesham Road": "For any Eastbound journeys, residents " +
+        "would need to take a high risk right turn through Warwick Road towards the A406 instead of " +
+        "travelling through Bounds Green.",
+    "Shrewsbury Road": "As York Road, Maidstone Road and Warwick Road are blocked off, residents here " +
+        "would need to travel through the A406 for all their journeys, and would " +
+        "need to take the risky right hand turn to Warwick Road. Journeys south of " +
+        "Bounds Green will be far more time consuming, and result in more fuel spent.",
+    "Maidstone Road": "As York Road, Maidstone Road and Warwick Road are blocked off, residents here " +
+        "would need to travel through the A406 for all their journeys, and would " +
+        "need to take the risky right hand turn to Warwick Road. Journeys south of " +
+        "Bounds Green will be far more time consuming, and result in more fuel spent. In addition, there will " +
+        "be more competition for parking spaces from residents on the south part of Maidstone Road, if they are " +
+        "not allowed to travel through Brownlow Road.",
+    "Tewkesbury Terrace": "Residents wishing to travel towards Southgate would no longer be able to do so via York Road" +
+        "/Brownlow Road, and would need to travel all the way round via Wood Green or Bowes Road. " +
+        "This is far more time consuming, and will lead to more fuel spent.",
+    "Russell Road": "Residents travelling towards Arnos Grove/Southgate would have a longer journey time, with " +
+        "more fuel spent, as Palmerston Road is blocked off.",
+    "Whittington Road": "Residents travelling towards Arnos Grove/Southgate would have a longer journey time, with " +
+        "more fuel spent, as Palmerston Road is blocked off.",
+    "Palmerston Road": "Residents travelling towards Arnos Grove/Southgate would have a longer journey time, with " +
+        "more fuel spent, as Palmerston Road is blocked off."
+};
